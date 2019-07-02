5dbd19d4b4103fbde689b294ebdabf3b1f97ae27
João Batista <jbdionisio@hotmail.com.br>
2019-07-02 14:56:09 -0300

    First commit

---
+++ src/main/java/com/example/dionisiomoney/api/DionisiomoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.dionisiomoney.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DionisiomoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DionisiomoneyApiApplication.class, args);
+	}
+
+}
