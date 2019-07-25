c1d7fa46491f1eedc9d2f2b8ca4a4f9425add469
João Batista <jbdionisio@hotmail.com.br>
2019-07-24 22:34:13 -0300

    First fetching data

--- src/main/java/com/example/dionisiomoney/api/resource/CategoryResource.java
+++ src/main/java/com/example/dionisiomoney/api/resource/CategoryResource.java
@@ -1,10 +1,25 @@
 package com.example.dionisiomoney.api.resource;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.dionisiomoney.api.model.Category;
+import com.example.dionisiomoney.api.repository.CategoryRepository;
 
 @RestController
 @RequestMapping("/categories")
 public class CategoryResource {
 
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	@GetMapping
+	public List<Category> listAll() {
+		return categoryRepository.findAll();
+	}
+	
 }
