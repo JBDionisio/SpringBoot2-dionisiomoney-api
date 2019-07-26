cbf7bf580c35239bf12a80098259e4bfff37a609
João Batista <jbdionisio@hotmail.com.br>
2019-07-25 21:26:26 -0300

    Request to Save a new category

--- src/main/java/com/example/dionisiomoney/api/resource/CategoryResource.java
+++ src/main/java/com/example/dionisiomoney/api/resource/CategoryResource.java
@@ -1,11 +1,18 @@
 package com.example.dionisiomoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.dionisiomoney.api.model.Category;
 import com.example.dionisiomoney.api.repository.CategoryRepository;
@@ -20,6 +27,17 @@ public class CategoryResource {
 	@GetMapping
 	public List<Category> listAll() {
 		return categoryRepository.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> save(@RequestBody Category category, HttpServletResponse response) {
+		Category savedCategory = categoryRepository.save(category);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+				.buildAndExpand(savedCategory.getId()).toUri();
+		response.addHeader("location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(savedCategory);
 	}
 	
 }
